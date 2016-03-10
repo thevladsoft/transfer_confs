4d0826bf56997d9270f01d9ae3b66fea4cab78e1
vidler <thevladsoft2@gmail.com>
2016-03-09 20:55:27 -0430

    Que no haga el rebase automaticamente

--- transfer_pull.sh
+++ transfer_pull.sh
@@ -2,7 +2,8 @@
 cd ${HOME}/mis_programas/transfer_confs/
 # timeout 30 git pull --rebase -f
 #pulling=$(timeout 30 git pull --rebase -f 2>&1)
-pulling=$(timeout 30 git pull --rebase 2>&1)
+#pulling=$(timeout 30 git pull --rebase 2>&1)
+pulling=$(timeout 30 git pull 2>&1)
 if [[ $? == 1  ]] || [[ "${pulling,,}" == *conflict* ]] || [[ "${pulling,,}" == *"cannot pull"* ]];then
 #    conflicto=true
    kdialog --passivepopup "<font color=red>Ocurrio un conflicto merging $(pwd)" 
